2ff19e266d3755d7ce60eec9a84aadd7c7bc32a9
Tarık Yılıkoğlu <tarik751@gmail.com>
2022-07-19 11:40:24 +0300

    Initial Commit

---
+++ Screen Time/Screen_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_TimeApp.swift
+//  Screen Time
+//
+//  Created by Tarık Yılıkoğlu on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Screen_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
